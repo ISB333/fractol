3bbd69232733fea34280459a5a453398c0a530a4
Alexandre Desille <adesille@k0r3p12.42mulhouse.fr>
2023-12-07 19:03:43 +0100

    07_12-17.30 = not working

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 12:52:57 by adesille          #+#    #+#             */
-/*   Updated: 2023/12/07 17:58:09 by adesille         ###   ########.fr       */
+/*   Updated: 2023/12/07 19:02:45 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,23 @@
 
 #define RED_PIXEL 0xFF0000
 #define GREEN_PIXEL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}		t_img;
 
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-}			t_data;
+	t_img	img;
+}		t_data;
 
 typedef struct s_rect
 {
@@ -48,26 +59,6 @@ typedef struct s_rect
 	int	height;
 	int	color;
 }		t_rect;
-
-/* The x and y coordinates of the rect corresponds to its upper left corner */
-
-int	render_rect(t_data *data, t_rect rect)
-{
-	int i;
-	int j;
-
-	if (data->win_ptr == NULL)
-		return (1);
-	i = rect.y;
-	while (i < rect.y + rect.height)
-	{
-		j = rect.x;
-		while (j < rect.x + rect.width)
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
-		++i;	
-	}
-	return (0);
-}
 
 int	handle_input(int keysym, t_data *data)
 {
@@ -87,10 +78,67 @@ int	handle_keypress(int keysym, t_data *data)
 	return(0);
 }
 
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
+
+	i = img->bpp - 8;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endianm LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+/* The x and y coordinates of the rect corresponds to its upper left corner */
+
+int	render_rect(t_img *img, t_rect rect)
+{
+	int i;
+	int j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;	
+	}
+	return (0);
+}
+
+void	render_background(t_img *img, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < W_HEIGHT)
+	{
+		j = 0;
+		while (j < W_WIDTH)
+			img_pix_put(img, j++, i, color);
+		++i;
+	}
+}
+
 int	render(t_data *data)
 {
-	render_rect(data, (t_rect){W_WIDTH - 100, W_HEIGHT - 100, 100, 100, GREEN_PIXEL});
-	render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	if (!data->win_ptr)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){W_WIDTH - 100, W_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rect(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 	return (0);
 }
 
@@ -116,6 +164,7 @@ int main(void)
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_hook(data.win_ptr, 17, 0, destroy_windows, &data);
 
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, W_WIDTH, W_HEIGHT);
 	mlx_loop(data.mlx_ptr); // loop that let the graphic server works
 
     /* we will exit the loop if there's no window left, and execute this code */
