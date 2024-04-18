6ec525ad046ca83cff1864f91b0a6ced2b472ad4
Alexandre Desille <adesille@k0r2p11.42mulhouse.fr>
2024-04-18 10:51:28 +0200

    2024-04-18 10:51

--- complex.c
+++ complex.c
@@ -6,7 +6,7 @@
 /*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 09:15:22 by adesille          #+#    #+#             */
-/*   Updated: 2024/04/18 10:34:07 by adesille         ###   ########.fr       */
+/*   Updated: 2024/04/18 10:42:59 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,29 @@ int	complex_calc(double r, double i, double cr, double ci)
 	ri[0] = 0;
 	cr = -0.123;
 	ci = 0.736;
-	while (++it < 9)
+	while (++it < 200)
 	{
 		r = ri[0] * ri[0] + ri[1] * ri[1] * -1 + cr;
 		i = ri[0] * ri[1] + ri[1] * ri[0] + ci;
 		ri[0] = r;
 		ri[1] = i;
-		printf("result = %f, %f\n", r, i);
 	}
+	if (ri[0] < 2 && ri[1] < 2)
+		printf("%f, %f IS STABLE\n", cr, ci);
+	else
+		printf("%f, %f Is UnStAbLeEeEe\n", cr, ci);
 	return (0);
 }
 
 int	main(void)
 {
 	complex_calc(0, 0, 0, 0);
+	double test = 0.123;
+	int i = -1;
+
+	while(++i < 5)
+	{
+		test += 0.0001;
+		printf("test %d = %f\n", i, test);
+	}
 }
