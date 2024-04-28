77a27121f5bbd768e2233eb0e3a43973c584720b
ISB333 <isb3@outlook.fr>
2024-04-28 12:00:01 +0200

    2024-04-28 12:00

--- r_include/rendering.c
+++ r_include/rendering.c
@@ -23,8 +23,16 @@ void	convert_values(double x, double y, t_coord **axis, float zoom)
 	y -= (HEIGHT / 2);
 
 	// Scale 
-	(*axis)->xr = x / 500 * zoom + (*axis)->x_zoom;
-	(*axis)->yi = y / 500 * zoom + (*axis)->y_zoom;
+	if ((*axis)->x_zoom || (*axis)->y_zoom)
+	{
+		(*axis)->xr = x / 500 * zoom + ((*axis)->x_zoom - (WIDTH / 2) / 500);
+		(*axis)->yi = y / 500 * zoom + ((*axis)->y_zoom - (HEIGHT / 2) / 500);
+	}
+	else
+	{
+		(*axis)->xr = x / 500;
+		(*axis)->yi = y / 500;
+	}
 }
 
 uint32_t shaders_to100(int startcolor, int endcolor, double len)
